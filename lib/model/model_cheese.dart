96fb3f9166e5514a058f93b7bbce4ef2922ea1c1
sophie <sophie.kim@luxrobo.com>
2020-09-17 18:22:45 +0900

    Add keyword variable

--- lib/model/model_cheese.dart
+++ lib/model/model_cheese.dart
@@ -1,12 +1,14 @@
 class Cheese {
   final String category;
   final String name;
+  final String keyword;
   final String image;
   final bool isLiked;
 
   Cheese.fromMap(Map<String, dynamic> map)
   : category = map['category'],
     name = map['name'],
+    keyword = map['keyword'],
     image = map['image'],
     isLiked = map['isLiked'];
 
